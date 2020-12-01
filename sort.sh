474d6e2b68306f9312d58d187f9acbd6d0eea883
Eile Kerning <tysayahi@gmail.com>
2020-12-01 12:02:58 -0500

    Update sort.sh

--- sort.sh
+++ sort.sh
@@ -7,13 +7,16 @@ git clone https://github.com/Osmium-Linux/rtl88-Themes-main.git
 cd rtl88-Themes-main
 RTDIR="$(pwd)"
 ### Do a bunch of sorting into various folders
-find . -type d -iname '*BlueAgave*' -exec cp -r {} $DIR/BlueAgave \;
-find . -type d -iname '*Kiwi*' -exec cp -r {} $DIR/Kiwi \;
-find . -type d -iname '*Plum*' -exec cp -r {} $DIR/Plum \;
-find . -type d -iname '*Strawberry*' -exec cp -r {} $DIR/Strawberry \;
-find . -type d -iname '*Tangerine*' -exec cp -r {} $DIR/Tangerine \;
-find . -type d -iname '*DEEP*' -exec cp -r {} $DIR/DEEP \;
-find . -type d -iname '*BLOOD*' -exec cp -r {} $DIR/BLOOD \;
-find . -type d -iname '*ENVY*' -exec cp -r {} $DIR/ENVY \;
-find . -type d -iname '*INK*' -exec cp -r {} $DIR/INK \;
-find . -type d -iname '*BLACK*' -exec cp -r {} $DIR/ \;
+find . -type d -iname '*BlueAgave*' -exec cp -r {} $DIR/AD-BlueAgave \;
+find . -type d -iname '*Kiwi*' -exec cp -r {} $DIR/AD-Kiwi \;
+find . -type d -iname '*Plum*' -exec cp -r {} $DIR/AD-Plum \;
+find . -type d -iname '*Strawberry*' -exec cp -r {} $DIR/AD-Strawberry \;
+find . -type d -iname '*AD-Tangerine*' -exec cp -r {} $DIR/AD-Tangerine \;
+find . -type d -iname '*DEEP*' -exec cp -r {} $DIR/Abyss-DEEP \;
+find . -type d -iname '*BLOOD*' -exec cp -r {} $DIR/Abyss-BLOOD \;
+find . -type d -iname '*ENVY*' -exec cp -r {} $DIR/Abyss-ENVY \;
+find . -type d -iname '*INK*' -exec cp -r {} $DIR/Abyss-INK \;
+find . -type d -iname '*Arc-BLACK*' -exec cp -r {} $DIR/Arc-Black \;
+find . -type d -iname '*Arc-BLACKEST*' -exec cp -r {} $DIR/Arc-Black \;
+find . -type d -iname '*Arc-Dark*' -exec cp -r {} $DIR/Arc-Dark \;
+
